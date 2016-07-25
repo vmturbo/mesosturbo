ddf7956cf8eb1ce5675ae5a81a9df43a262a5520
pamelasanchezvi <pamela.aimee.sanchez@gmail.com>
2016-07-25 14:40:02 -0700

    fixed glog message

--- main/main.go
+++ main/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	goflag "flag"
 	"fmt"
-	"github.com/golang/glog"
 	"github.com/spf13/pflag"
 	"github.com/vmturbo/mesosturbo/communicator/metadata"
 	"github.com/vmturbo/mesosturbo/communicator/vmtapi"
@@ -13,30 +12,36 @@ import (
 
 func init() {
 	goflag.Set("logtostderr", "true")
-
 	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 	pflag.Parse()
 }
 
 func main() {
+
 	var actions string
-	glog.Infof("In main :)")
+	fmt.Printf("In main :)")
+
 	metadata, err := metadata.NewVMTMeta("../communicator/metadata/config.json")
+
 	if err != nil {
-		glog.Infof("Error!! : %s\n", err)
+		fmt.Printf("Error!! : %s\n", err)
 	}
+
 	mesosClient := &action.MesosClient{
 		MesosMasterIP:   metadata.MesosActionIP,
 		MesosMasterPort: metadata.MesosActionPort,
 		Action:          "",
 	}
+
 	fmt.Printf("----> metadata is %+v", metadata)
+
 	// Pending Task watcher
 	// TODO flag
 	actions = "disabled"
 	if actions == "enabled" {
 		go api.CreateWatcher(mesosClient, metadata)
 	}
+
 	clientMap := make(map[string]string)
 	comm := vmturbocommunicator.NewVMTCommunicator(clientMap, metadata)
 	comm.Run()
